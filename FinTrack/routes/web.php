27a32d2f9e269397750a67e896806cb07dd29870
puja2tib <puja24ti@mahasiswa.pcr.ac.id>
2025-10-03 17:36:57 +0700

    second commit

--- FinTrack/routes/web.php
+++ FinTrack/routes/web.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/signup', [SignupController::class, 'showSignupForm'])->name('signup');
+Route::get('/signup', [SignupController::class, 'index'])->name('signup');
 Route::post('/signup', [SignupController::class, 'signup']);
